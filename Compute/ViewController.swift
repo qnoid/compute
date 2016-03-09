3717203dba27bc7e57a63bbae9176b7e50fffab7
Markos Charatzas <markos@qnoid.com>
2016-03-09 22:42:06 +0000

    init

---
+++ Compute/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Compute
+//
+//  Created by Markos Charatzas on 09/03/2016.
+//  Copyright © 2016 Markos Charatzas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
